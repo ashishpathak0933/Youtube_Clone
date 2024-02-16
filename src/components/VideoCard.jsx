dddb271c29e26c9b8aaa8153a6c1745d6ebf9d94
Ashish kumar pathak <ashishpathak0933@gmail.com>
2024-02-16 21:37:55 +0100

    channel card added

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,23 +3,17 @@ import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle,
-     demoChannelUrl, demoChannelTitle } from "../utils/constants";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
-const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
-    
-  return (
-    <Card  sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, 
-    boxShadow: "none", 
-    borderRadius: 0 }}
-    >
-     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
-    <CardMedia  image={snippet?.thumbnails?.high?.url 
-    || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }}/>
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
+  <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
+      <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
+        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
+      />
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
-    <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
@@ -31,8 +25,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
         </Typography>
       </Link>
     </CardContent>
-    </Card>
-  )
-}
+  </Card>
+);
 
 export default VideoCard
